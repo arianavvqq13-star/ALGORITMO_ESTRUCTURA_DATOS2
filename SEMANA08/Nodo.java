ae03a97c3da5522fcc118c60482ab8a7a82ab8fa
arianavvqq13-star <ariana.vv.qq13@gmail.com>
2026-05-21 15:07:41 -0500

    Create Nodo.java

---
+++ SEMANA08/Nodo.java
@@ -0,0 +1,15 @@
+
+package Libreria;
+
+
+public class Nodo {
+
+    int dato;
+    Nodo siguiente;
+
+    // Constructor
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
